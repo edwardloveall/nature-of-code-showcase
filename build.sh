646b64f1b9cb95fc75c8aad75f9e38cbd3b7b84d
Edward Loveall <edward@edwardloveall.com>
2013-08-18 17:38:33 -0400

    Build script now pushes to github automatically

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 rm -rf ./build/
 middleman build
 rm -rf ./build/stylesheets/bourbon/
+mv build /tmp/build
+git checkout gh-pages
+mv /tmp/build/* ./
+git add .
+git commit -m "Updated gh-pages"
+git push origin gh-pages
+git checkout master
