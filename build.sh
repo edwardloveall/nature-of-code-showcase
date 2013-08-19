f57c02db5d1913ecab4380b55608c3c41daf0ea7
Edward Loveall <edward@edwardloveall.com>
2013-08-18 23:09:30 -0400

    new build script

--- build.sh
+++ build.sh
@@ -2,3 +2,10 @@ rm -rf /tmp/build/
 middleman build
 rm -rf ./build/stylesheets/bourbon/
 mv build /tmp/build
+git checkout gh-pages
+rm -rf ./*
+mv -f /tmp/build/* ./
+git add .
+git commit -m "Updating gh-pages"
+git push origin gh-pages
+git checkout master
